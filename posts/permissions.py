160cb9a46e83aa172f1247d595bdc3b93c09d8c8
blktheta <blktheta@gmail.com>
2023-08-01 21:20:20 +0200

    Add API permissions

---
+++ posts/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def har_permission(self, request, view):
+        # Authenticated users cant only see list view
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        # always allow GET, HEAD or OPTIONS requests
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the author of a post
+        return obj.author == request.user
